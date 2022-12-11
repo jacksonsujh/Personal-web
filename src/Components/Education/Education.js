4b42ea471cc24b8417ba68db52d8e39fb0138780
jacksonsujh <72714023+jacksonsujh@users.noreply.github.com>
2022-12-11 00:07:19 -0500

    Reformatted the school description in SchoolInfo component

--- src/Components/Education/Education.js
+++ src/Components/Education/Education.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
+import Moscrop from "../../Statics/Moscrop.JPG";
+import ISPP from "../../Statics/ISPP.JPG";
+
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
@@ -16,41 +19,54 @@ import {SchoolInfo} from "../SchoolInfo/SchoolInfo";
 
 
 const Schools = [{
-        "ImageURL":"",
+        "ImageURL": "",
         "Name": "University of Toronto",
         "Title":"Software developer",
         "Location": "Toronto, Canada",
         "Intro":""
     },
     {
-        "ImageURL":"",
+        "ImageURL":Moscrop,
         "Name": "Moscrop Secondary School",
         "Title":"President of the SUA, since 2019",
         "Location": "Vancouver, Canada",
-        "Intro":"Received Principle's List Award During G12\n" +
-            "\n" +
-            "Challenging myself by taking various AP courses, such as AP Microeconomics, AP Macroeconomic, AP Physics 1 and AP Chinese. Graduated with best 6 subjects average of 94.6%\n" +
-            "\n" +
-            "Founded Student United Association, focusing on promoting friendship between international and local students and cross-cultural communication, increasing school-to-school connections. Recruited 17 members and initiated the first \"New Year Party\" at Moscrop large gym, inviting over 100 students and raised $1500.\n" +
-            "\n" +
-            "Joined Moscrop Student Government in 2020 as External Relations Rep. Responsible for sponsorship and planning school-wide events, including the Winter Formal, Townhall, Games night, etc.\n" +
-            "\n" +
-            "Participated in Ronald McDonald House BC & Yucon high school volunteer program. Initiated \"Walk for Live\" fundraiser, raising for children from disadvantaged families, who are hospitalized in Vancouver."
+        "Intro": <p>
+            Received Principle's List Award During G12
+            <br/>
+            <br/>
+            Challenging myself by taking various AP courses, such as AP Microeconomics, AP Macroeconomic, AP Physics 1 and AP Chinese. Graduated with best 6 subjects average of 94.6%
+            <br/>
+            <br/>
+            Founded Student United Association, focusing on promoting friendship between international and local students and cross-cultural communication, increasing school-to-school connections. Recruited 17 members and initiated the first "New Year Party" at Moscrop large gym, inviting over 100 students and raised $1500.
+            <br/>
+            <br/>
+            Joined Moscrop Student Government in 2020 as External Relations Rep. Responsible for sponsorship and planning school-wide events, including the Winter Formal, Townhall, Games night, etc.
+            <br/>
+            <br/>
+            Participated in Ronald McDonald House BC & Yucon high school volunteer program. Initiated "Walk for Live" fundraiser, raising for children from disadvantaged families, who are hospitalized in Vancouver.
+        </p>
     },
     {
-        "ImageURL":"",
+        "ImageURL":ISPP,
         "Name": "International School of Phnom Penh",
         "Title":"IB MYP degree, 2019",
         "Location": "Phnom Penh, Cambodia",
-        "Intro":"Courses taken: English, Chinese, Digital Design, Music, Mathematics, Individual & Society\n" +
-            "\n" +
-            "Personal Project: Teach local children and schoolmates my own choreography, passed on the culture and spirit of dance, feel the power and happiness brought by it, and finally performed in the final assembly of the school.\n" +
-            "\n" +
-            "Participated in MARISA swimming team\n" +
-            "Specialized in freestyle and breaststroke; Attended regular training(2-3 times a week); won the first prize in the ISSAPP competition.\n" +
-            "\n" +
-            "Participated in MARISA volleyball team\n" +
-            "Attended weekly practices(3 times a week); won the first prize in the ISSAPP competition."
+        "Intro": <p>
+            Courses taken: English, Chinese, Digital Design, Music, Mathematics, Individual & Society
+            <br/>
+            <br/>
+            Personal Project: Teach local children and schoolmates my own choreography, passed on the culture and spirit of dance, feel the power and happiness brought by it, and finally performed in the final assembly of the school.
+            <br/>
+            <br/>
+            Participated in MARISA swimming team
+            <br/>
+            Specialized in freestyle and breaststroke; Attended regular training(2-3 times a week); won the first prize in the ISSAPP competition.
+            <br/>
+            <br/>
+            Participated in MARISA volleyball team
+            <br/>
+            Attended weekly practices(3 times a week); won the first prize in the ISSAPP competition.
+        </p>
     }
 ];
 
--- src/Components/SchoolInfo/SchoolInfo.js
+++ src/Components/SchoolInfo/SchoolInfo.js
@@ -5,9 +5,9 @@ import "./SchoolInfo.css";
 export class SchoolInfo extends React.Component{
     render() {
         return(
-            <div>
+            <div className="school">
                 <img src={this.props.url} alt={this.props.school.Name}/>
-                <div>
+                <div className="schoolDescription">
                     <h1>{this.props.school.Name}</h1>
                     <h2>{this.props.school.Title}</h2>
                     <h3>{this.props.school.Location}</h3>
